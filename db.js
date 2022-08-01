010db41c6117671dfd0d2c5b982b4738298ad15e
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-08-01 14:50:35 +0200

    exported mysql db to ready.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
-const Database = require('./classes/Database.js')
-const emojiquiz = new Database();
-emojiquiz.host = "localhost";
-emojiquiz.user = "root";
-emojiquiz.password = "";
-emojiquiz.database = "emojiquiz";
-emojiquiz.charset = 'utf8mb4';
 
-emojiquiz.createTable();
+const Database = require('./classes/Database.js')
+const emojiquizMYSQL = new Database();
+emojiquizMYSQL.host = "localhost";
+emojiquizMYSQL.user = "root";
+emojiquizMYSQL.password = "";
+emojiquizMYSQL.database = "emojiquiz";
+emojiquizMYSQL.charset = 'utf8mb4';
+module.exports = {emojiquizMYSQL};
--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,9 @@
-const Database = require('./classes/Database.js')
+const { emojiquizMYSQL } = require('../db.js');
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
+		emojiquizMYSQL.createTable();
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
