f99554bff5c0890afab03a0249d977c95683782d
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-08-04 15:21:16 +0200

    added presence to ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,18 @@
 const { emojiquiz } = require('../db.js');
+const { ActivityType } = require('discord.js');
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
 		emojiquiz.ready();
+		setInterval(() => {
+		const arr = ['Coded by Jeezy#1111', 'JeezyDevelopment package', 'discord-emojiquiz'];
+		let shuffledNumbers = arr.sort(function () {
+			return Math.random() - 0.5;
+		  });
+		
+		client.user.setPresence({ activities: [{ type: ActivityType.Listening, name: shuffledNumbers[0] }], status: 'online' });
+	}, 60000);
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
